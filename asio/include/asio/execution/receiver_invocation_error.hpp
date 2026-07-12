2300b7a0684cf8c390b78ca67368242985f502f6
Gleb Mazovetskiy <glex.spb@gmail.com>
2026-07-12 14:06:01 +0100

    Add missing #include <exception>
    `std::nested_exception` and `std::terminate` are declared in
    `<exception>`.

--- asio/include/asio/execution/receiver_invocation_error.hpp
+++ asio/include/asio/execution/receiver_invocation_error.hpp
@@ -15,9 +15,10 @@
 # pragma once
 #endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
-#include "asio/detail/config.hpp"
+#include <exception>
 #include <stdexcept>
 
+#include "asio/detail/config.hpp"
 #include "asio/detail/push_options.hpp"
 
 namespace asio {
